aea4cd180dbf76f3b753eaafc8576a944b954fa8
Yusifx1 <63213937+Yusifx1@users.noreply.github.com>
2020-04-06 15:04:23 +0400

    Add files via upload

--- Set.sh
+++ Set.sh
@@ -2,11 +2,9 @@
 count=1
 echo "Specify the id or shortname of the set you would like to download"
 read set
-if ! [[ -d $set ]]; then
-mkdir $set
-else
-echo "updating set $set"
-fi
+
+name=`curl -# -A "e621downloader/1.1 (Yusifx1/e621-scripts)" "https://e621.net/post_sets/$set.json" | jq -r '.name'`
+echo "Downloading $name set" 
 
 curl -s -A "MyProject/1.0 (new api is shit)"  "https://e621.net/posts.json?tags=set%3A$set+order%3Aid" |jq -r '.posts[]|.file.url' >url
 while read l
@@ -21,6 +19,6 @@ elif [[ $l =~ ^.*webm.*$ ]]; then
 e=webm
 else echo Corrupted link!!!
 fi
-curl -o "${set}/${count}.${e}" -C - $l
+curl --create-dirs -o "${name}/${count}.${e}" -C - $l
 count=$((count+1))
 done <url
