1191bcfc60c7d96a8cfb2379807da2b0689861b1
Yusifx1 <63213937+Yusifx1@users.noreply.github.com>
2020-04-06 13:37:44 +0400

    Update Set.sh

--- Set.sh
+++ Set.sh
@@ -1,4 +1,26 @@
+savedir=name   #Save file in folder named by id or name
+
 count=1
+echo "Specify the id or shortname of the set you would like to download"
+read set
+if [[ $savedir = id ]]; then
+if ! [[ -d $set ]]; then
+mkdir $set
+else
+echo "updating pool $set"
+fi
+
+elif [[ $savedir = name ]]; then
+name=`curl -# -A "e621downloader/1.1 (Yusifx1/e621-scripts)" "https://e621.net/post_sets/$set.json" | jq -r '.name'`
+if ! [[ -d $name ]]; then
+mkdir $name
+savedir="$name"
+else
+savedir="$name"
+echo "updating pool $name"
+fi
+fi
+
 echo "Specify the name of the set you would like to download"
 read set
 curl -s -A "MyProject/1.0 (new api is shit)"  "https://e621.net/posts.json?tags=set%3A$set+order%3Aid" |jq -r '.posts[]|.file.url' >url
@@ -14,6 +36,6 @@ elif [[ $l =~ ^.*webm.*$ ]]; then
 e=webm
 else echo Corrupted link!!!
 fi
-curl -o "${count}.${e}" -C - $l
+curl -o "${savedir}/${count}.${e}" -C - $l
 count=$((count+1))
 done <url
