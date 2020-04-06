0d6392afc4c0b7d1199dd62369fcdc93725b2792
Yusifx1 <63213937+Yusifx1@users.noreply.github.com>
2020-04-06 15:07:38 +0400

    Update Set.sh

--- Set.sh
+++ Set.sh
@@ -6,7 +6,7 @@ read set
 name=`curl -# -A "e621downloader/1.1 (Yusifx1/e621-scripts)" "https://e621.net/post_sets/$set.json" | jq -r '.name'`
 echo "Downloading $name set" 
 
-curl -s -A "MyProject/1.0 (new api is shit)"  "https://e621.net/posts.json?tags=set%3A$set+order%3Aid" |jq -r '.posts[]|.file.url' >url
+curl -s -A "e621-downloader/1.1 (Yusifx1/e621-scripts)"  "https://e621.net/posts.json?tags=set%3A$set+order%3Aid" |jq -r '.posts[]|.file.url' >url
 while read l
 do 
 if [[ $l =~ ^.*gif.*$ ]]; then
