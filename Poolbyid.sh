27919921fefba26f7d39afbfc6f644f5429f8165
Yusifx1 <63213937+Yusifx1@users.noreply.github.com>
2020-04-08 16:27:25 +0400

    Added apikey and download dir

--- Poolbyid.sh
+++ Poolbyid.sh
@@ -1,22 +1,27 @@
 count=1
 echo "Specify the ID of the pool you would like to download"
-read pid
-bid=`curl -# -A "MyProject/1.0 (new api is shit)" "https://e621.net/pools/$pid.json" | jq -c '.post_ids'`
-id=`echo ${bid:1:-1}`
-curl -s -A "MyProject/1.0 (new api is shit)" https://e621.net/posts/{$id}.json | jq -r '.post.file.url' |sed "/null/d"  >url
+read id
+name=`curl -A "e621downloader/1.2 (Yusifx1/e621-scripts)" "https://e621.net/pools/$id.json" | jq -r '.name'`
+pid=`curl -# -A "e621downloader/1.2 (Yusifx1/e621-scripts)" "https://e621.net/pools/$id.json" | jq -c '.post_ids'`
+pid=`echo ${pid:1:-1}`
+dir=$name
+source base.cfg
+echo "Downloading to $dir" 
+curl -s -A "e621downloader/1.2 (Yusifx1/e621-scripts)" "https://e621.net/posts/{$pid}.json?$log&$api" | jq -r '.post.file.url' |sed "/null/d"  >url
  while read l
 do 
-if [[ $l =~ ^.*gif.*$ ]]; then
+if [[ $l =~ ^.*gif ]]; then
 e=gif
-elif [[ $l =~ ^.*jpg.*$ ]]; then
+elif [[ $l =~ ^.*jpg ]]; then
 e=jpg
-elif [[ $l =~ ^.*png.*$ ]]; then
+elif [[ $l =~ ^.*png ]]; then
 e=png
-elif [[ $l =~ ^.*webm.*$ ]]; then
+elif [[ $l =~ ^.*webm ]]; then
 e=webm
 else 
 echo Corrupted link!!!
 fi
-curl --create-dirs -o "${pid}/${count}.${e}" -C - $l
+curl --create-dirs -o "$dir/${count}.$e" -C - $l
 count=$((count+1))
 done <url
+ <url
