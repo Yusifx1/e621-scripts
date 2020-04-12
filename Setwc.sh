079ea49ce4dfc9d831af777350789af7e3f13a7c
Yusifx1 <63213937+Yusifx1@users.noreply.github.com>
2020-04-12 20:14:49 +0400

    This script aliased with Set.sh

--- Setwc.sh
+++
@@ -1,5 +0,0 @@
-echo "Specify the name of the set you would like to download"
-read set
-curl -s -A "MyProject/1.0 (new api is shit)"  "https://e621.net/posts.json?tags=set%3A$set+order%3Aid" |jq -r '.posts[]|.file.url' >url
-
-xargs -n 1 curl -O -C - <url
