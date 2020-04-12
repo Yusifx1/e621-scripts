9583de79b9570bf5b704d6dedd49c3b1a5339f01
Yusifx1 <63213937+Yusifx1@users.noreply.github.com>
2020-04-12 20:14:05 +0400

    New version 1.3
    Aliased with fast method.
    Deleted code that need't.

--- Poolbyid.sh
+++ Poolbyid.sh
@@ -1,13 +1,26 @@
 count=1
 echo "Specify the ID of the pool you would like to download"
 read id
-name=`curl -A "e621downloader/1.2 (Yusifx1/e621-scripts)" "https://e621.net/pools/$id.json" | jq -r '.name'`
-pid=`curl -# -A "e621downloader/1.2 (Yusifx1/e621-scripts)" "https://e621.net/pools/$id.json" | jq -c '.post_ids'`
+name=`curl -A "e621downloader/1.3 (Yusifx1/e621-scripts)" "https://e621.net/pools/$id.json" | jq -r '.name'`
+pid=`curl -# -A "e621downloader/1.3 (Yusifx1/e621-scripts)" "https://e621.net/pools/$id.json" | jq -c '.post_ids'`
 pid=`echo ${pid:1:-1}`
+dwo=no
 dir=$name
+p=1
+lc=320
 source base.cfg
 echo "Downloading to $dir" 
-curl -s -A "e621downloader/1.2 (Yusifx1/e621-scripts)" "https://e621.net/posts/{$pid}.json?$log&$api" | jq -r '.post.file.url' |sed "/null/d"  >url
+if [[ $dwo = yes ]]; then
+curl -s -A "e621downloader/1.3 (Yusifx1/e621-scripts)" "https://e621.net/posts/{$pid}.json?$log&$api" | jq -r '.post.file.url' |sed "/null/d"  >url
+else
+while [ $lc = 320 ] 
+do
+curl -s -A "e621-downloader/1.3 (Yusifx1/e621-scripts)"  "https://e621.net/posts.json?tags=pool%3A$id+order%3Aid&$log&$api&limit=320&page=$p" |jq -r '.posts[]|.file.url' >>url
+l=`wc -l url | cut -f1 -d' '`
+lc=`echo $((l/p))`
+p=$((p+1))
+done
+fi
  while read l
 do 
 if [[ $l =~ ^.*gif ]]; then
@@ -23,5 +36,4 @@ echo Corrupted link!!!
 fi
 curl --create-dirs -o "$dir/${count}.$e" -C - $l
 count=$((count+1))
-done <url
- <url
+done <url && rm url
--- Set.sh
+++ Set.sh
@@ -1,24 +1,40 @@
 
 count=1
 echo "Specify the id or shortname of the set you would like to download"
-read set
-
-name=`curl -# -A "e621downloader/1.1 (Yusifx1/e621-scripts)" "https://e621.net/post_sets/$set.json" | jq -r '.name'`
-echo "Downloading $name set" 
-
-curl -s -A "e621-downloader/1.1 (Yusifx1/e621-scripts)"  "https://e621.net/posts.json?tags=set%3A$set+order%3Aid" |jq -r '.posts[]|.file.url' >url
+read id
+name=`curl -# -A "e621downloader/1.3 (Yusifx1/e621-scripts)" "https://e621.net/post_sets/$id.json" | jq -r '.name'`
+echo "Getting url of $name set" 
+dir=$name
+sc=yes
+source base.cfg
+p=1
+lc=320
+while true;do echo -n .;sleep 1;done & 
+while [ $lc = 320 ] 
+do
+curl -s -A "e621-downloader/1.3 (Yusifx1/e621-scripts)"  "https://e621.net/posts.json?tags=set%3A$id+order%3Aid&limit=320&$log&$api&page=$p" |jq -r '.posts[]|.file.url' >>url
+l=`wc -l url | cut -f1 -d' '`
+lc=`echo $((l/p))`
+p=$((p+1))
+done
+kill $!; 
+if [[ $sc = no ]]; then
+xargs -n 1 curl -O -C - <url
+else
 while read l
 do 
-if [[ $l =~ ^.*gif.*$ ]]; then
+if [[ $l =~ ^.*gif ]]; then
 e=gif
-elif [[ $l =~ ^.*jpg.*$ ]]; then
+elif [[ $l =~ ^.*jpg ]]; then
 e=jpg
-elif [[ $l =~ ^.*png.*$ ]]; then
+elif [[ $l =~ ^.*png ]]; then
 e=png
-elif [[ $l =~ ^.*webm.*$ ]]; then
+elif [[ $l =~ ^.*webm ]]; then
 e=webm
 else echo Corrupted link!!!
 fi
-curl --create-dirs -o "${name}/${count}.${e}" -C - $l
+curl --create-dirs -o "$dir/${count}.${e}" -C - $l
 count=$((count+1))
 done <url
+fi
+rm url
